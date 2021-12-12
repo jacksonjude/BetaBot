ebcc987bcbc23680ef318bfc8caec9ebd8356ec5
jacksonjude <jacksonjude@mac.com>
2021-12-12 00:27:25 -0800

    Added server message counts support via the stats config; Moved online member count updates to cron jobs

--- src/serverStats.js
+++ src/serverStats.js
@@ -1,8 +1,12 @@
-// Member Stats
+import { CronJob } from "cron"
+
+const statChannelsCollectionID = "statsConfigurations"
 
 var statsData = {}
+var onlineMemberCountCronJob
+var messageCountsCronJobs = {}
 
-export const interpretStatsSetting = async function(client, guildID, statsDataJSON)
+export const interpretStatsSetting = async function(client, guildID, statsDataJSON, firestoreDB)
 {
   statsData[guildID] = statsDataJSON
 
@@ -11,6 +15,12 @@ export const interpretStatsSetting = async function(client, guildID, statsDataJS
     updateOnlineMembersStat(guild)
     updateBoostMembersStat(guild)
   }).catch(console.error)
+
+  if (statsData[guildID].messageCounts)
+  {
+    let messageCountsData = statsData[guildID].messageCounts
+    setupMessageCountsCronJob(client, guildID, messageCountsData, firestoreDB)
+  }
 }
 
 export const setupMemberStatsEventHandlers = function(client)
@@ -27,17 +37,37 @@ export const setupMemberStatsEventHandlers = function(client)
     updateTotalMembersStat(member.guild)
   })
 
-  client.on('presenceUpdate', (member) => {
-    // Update online stat
-    if (member == null || member.guild == null) { return }
-    updateOnlineMembersStat(member.guild) // Not using this for updates
+  onlineMemberCountCronJob = new CronJob("0 */10 * * * *", () => {
+    Object.keys(statsData).forEach((guildID) => {
+      client.guilds.fetch(guildID).then(guild => {
+        updateOnlineMembersStat(guild)
+      })
+    })
   })
+  onlineMemberCountCronJob.start()
 
   client.on('guildMemberUpdate', async (oldMember, _) => {
     // Update boost stat
     if (oldMember == null || oldMember.guild == null) { return }
     updateBoostMembersStat(oldMember.guild)
   })
+}
+
+function setupMessageCountsCronJob(client, guildID, messageCountsData, firestoreDB)
+{
+  let messageCountCronJob = new CronJob("1 0 " + (messageCountsData.cronHours ?? "0") + " * * *", () => {
+    client.guilds.fetch(guildID).then(async guild => {
+      updateMessageCounts(guild, messageCountsData.hours, messageCountsData.startTime.toMillis(), firestoreDB)
+    })
+  }, null, true, messageCountsData.timeZone ?? "America/Los_Angeles")
+  messageCountCronJob.start()
+
+  if (messageCountsCronJobs[guildID])
+  {
+    messageCountsCronJobs[guildID].stop()
+  }
+
+  messageCountsCronJobs[guildID] = messageCountCronJob
 }
 
 function updateTotalMembersStat(guild)
@@ -54,12 +84,6 @@ async function updateOnlineMembersStat(guild)
 {
   var guildStatsSettings = statsData[guild.id]
   if (guildStatsSettings == null || guildStatsSettings.onlineCountChannelID == null) { return }
-
-  if (guildStatsSettings.lastOnlineCountFetch != null && Date.now()-guildStatsSettings.lastOnlineCountFetch < 1000*60*10)
-  {
-    return
-  }
-  guildStatsSettings.lastOnlineCountFetch = Date.now()
 
   var guildMembers = await guild.members.fetch()
   var onlineCount = guildMembers.filter(m => m.presence != null && m.presence.status != "offline").size
@@ -85,4 +109,87 @@ async function updateStatChannelName(guild, channelID, statValue)
   let currentChannelName = channelToUpdate.name
   let newChannelName = currentChannelName.replace(/\d+/, statValue)
   await channelToUpdate.setName(newChannelName)
+}
+
+async function updateMessageCounts(guild, hoursPerSegment, trackingStartTime, firestoreDB)
+{
+  let messageCountsCollectionPath = statChannelsCollectionID + "/" + guild.id + "/" + "messageCounts"
+  let previousMessageCounts = await firestoreDB.collection(messageCountsCollectionPath).get()
+
+  let previouslyCalculatedSegments = []
+  previousMessageCounts.forEach((messageCountsDoc) => {
+    previouslyCalculatedSegments.push(messageCountsDoc.id)
+  })
+
+  var updatedMessageCountSegments = {}
+
+  var guildChannels = await guild.channels.fetch()
+  for (let channel of guildChannels.values())
+  {
+    if (channel.type != "GUILD_TEXT") { continue }
+    let channelMessages
+    try
+    {
+      channelMessages = await channel.messages.fetch({limit: 100})
+    }
+    catch (error)
+    {
+      continue
+    }
+
+    const processMessages = async function(messages)
+    {
+      for (let message of messages)
+      {
+        let messageCreatedTimestamp = message.createdAt.getTime()
+        if (messageCreatedTimestamp < trackingStartTime) { return true }
+        if (message.author.bot) { continue }
+
+        let segmentStartTime = messageCreatedTimestamp-((messageCreatedTimestamp-trackingStartTime)%(hoursPerSegment*60*60*1000))
+        if (previouslyCalculatedSegments.includes(segmentStartTime))
+        {
+          return true
+        }
+
+        if (!updatedMessageCountSegments[segmentStartTime])
+        {
+          updatedMessageCountSegments[segmentStartTime] = {}
+        }
+        if (!updatedMessageCountSegments[segmentStartTime][message.author.id])
+        {
+          updatedMessageCountSegments[segmentStartTime][message.author.id] = 0
+        }
+
+        updatedMessageCountSegments[segmentStartTime][message.author.id] += 1
+      }
+
+      return false
+    }
+
+    let shouldBreakMessageLoop = false
+    // let fetchedMessageCount = channelMessages.size
+    while (channelMessages.size > 0 && !shouldBreakMessageLoop)
+    {
+      // console.log("Message Counts: " + channel.id + " (count = " + fetchedMessageCount + ", time = " + channelMessages.last().createdAt.getTime() + ")")
+      shouldBreakMessageLoop = await processMessages(channelMessages.values())
+
+      if (channelMessages.size > 0 && channelMessages.last().id)
+      {
+        try
+        {
+          channelMessages = await channel.messages.fetch({before: channelMessages.last().id, limit: 100})
+          // fetchedMessageCount += channelMessages.size
+        }
+        catch
+        {
+          shouldBreakMessageLoop = true
+        }
+      }
+    }
+  }
+
+  for (let messageCountSegmentTime in updatedMessageCountSegments)
+  {
+    await firestoreDB.doc(messageCountsCollectionPath + "/" + messageCountSegmentTime).set(updatedMessageCountSegments[messageCountSegmentTime])
+  }
 }
